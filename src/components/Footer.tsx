41f6f8552596ed4aeb91580042bc9ba7d6c0a9e8
Thiago Medeiros <thiagotitico1@gmail.com>
2026-04-10 18:59:08 -0300

    Subindo o footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,33 @@
+import "./styles/Footer.css"
+
 export default function Footer() {
     return(
-    <footer>
-        <h1>FOOTER</h1>
+    <footer className="Footer">
+        <div className="Footer_itens">
+            <div className="Footer_coluna">
+                <FooterLink href="#" text="Página Inicial"/>
+                <FooterLink href="#" text="Sobre nós"/>
+                <FooterLink href="#" text="Serviços"/>
+                <FooterLink href="#" text="Preços"/>
+                <FooterLink href="#" text="Contato"/>
+            </div>
+            <div className="Footer_coluna">
+                <FooterLink href="#" text="Termos e condições"/>
+                <FooterLink href="#" text="Politicas de privacidade"/>
+                <FooterLink href="#" text="Cookies"/>
+            </div>
+        </div>
     </footer>
+    )
+}
+
+interface FooterLinkProps {
+    href: string;
+    text: string;
+}
+
+function FooterLink({href, text} : FooterLinkProps) {
+    return(
+        <a href={href}>{text}</a>
     )
 }
