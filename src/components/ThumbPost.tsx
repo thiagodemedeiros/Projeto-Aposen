040ef4c7709b7507fd024be77720dec1e79036a6
Thiago Medeiros <thiagotitico1@gmail.com>
2026-04-10 18:43:51 -0300

    o component thumb agora tem link para facilitar a rotação do site

--- src/components/ThumbPost.tsx
+++ src/components/ThumbPost.tsx
@@ -3,28 +3,33 @@ import "./styles/ThumbPost.css";
 interface ThumbPostProps {
     titulo: string;
     noticia: string;
+    link: string;
 }
 
-export function ThumbPostVertical({titulo, noticia} : ThumbPostProps) {
+export function ThumbPostVertical({titulo, noticia, link} : ThumbPostProps) {
     return(
-        <div className="ThumbPostVertical">
-            <img src="https://img.freepik.com/fotos-gratis/fundo-abstrato-escuro_1048-1920.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
-            <div className="ThumbPostVertical_texto">
-                <h3>{titulo}</h3>
-                <p>{noticia}</p>
+        <a href={link}>
+            <div className="ThumbPostVertical">
+                <img src="https://img.freepik.com/fotos-gratis/fundo-abstrato-escuro_1048-1920.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
+                <div className="ThumbPostVertical_texto">
+                    <h3>{titulo}</h3>
+                    <p>{noticia}</p>
+                </div>
             </div>
-        </div>
+        </a>
     )
 }
 
-export function ThumbPostHorizontal({titulo, noticia} : ThumbPostProps) {
+export function ThumbPostHorizontal({titulo, noticia, link} : ThumbPostProps) {
     return(
-        <div className="ThumbPostHorizontal">
-            <img src="https://img.freepik.com/fotos-gratis/fundo-abstrato-escuro_1048-1920.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
-            <div className="ThumbPostHorizontal_texto">
-                <h3>{titulo}</h3>
-                <p>{noticia}</p>
+        <a href={link}>
+            <div className="ThumbPostHorizontal">
+                <img src="https://img.freepik.com/fotos-gratis/fundo-abstrato-escuro_1048-1920.jpg?semt=ais_hybrid&w=740&q=80" alt="" />
+                <div className="ThumbPostHorizontal_texto">
+                    <h3>{titulo}</h3>
+                    <p>{noticia}</p>
+                </div>
             </div>
-        </div>
+        </a>
     )
 }
