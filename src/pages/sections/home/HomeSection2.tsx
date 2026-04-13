1ecd3946a77863ada5f35da1836e9afd18da56f0
Thiago Medeiros <thiagotitico1@gmail.com>
2026-04-13 01:33:33 -0300

    mudando as imagens da section 2 da home para que fique exatamente como planejado pelo designer

--- src/pages/sections/home/HomeSection2.tsx
+++ src/pages/sections/home/HomeSection2.tsx
@@ -1,18 +1,18 @@
 import "./styles/HomeSection2.css"
-import ThemeIcons from "../../../components/ThemeIcons"
 import balance from "./assets/balance.png"
 import dollar from "./assets/dollar.png"
 import handshake from "./assets/handshake.png"
 import healthCare from "./assets/health-care.png"
+import { Link } from "react-router-dom"
 
 export default function HomeSection2() {
     return (
         <div className="HomeSection2">
             <div className="HomeSection2_container">
-                <ThemeIcons link="/search/type/tech" img={healthCare} />
-                <ThemeIcons link="/search/type/tech" img={dollar} />
-                <ThemeIcons link="/search/type/design" img={handshake} />
-                <ThemeIcons link="/search/type/design" img={balance} />
+                <Link to="/search/type/tech"><img src={healthCare} alt="" /></Link>
+                <Link to="/search/type/tech"><img src={dollar} alt="" /></Link>
+                <Link to="/search/type/design"><img src={handshake} alt="" /></Link>
+                <Link to="/search/type/design"><img src={balance} alt="" /></Link>
             </div>
         </div>
     );
