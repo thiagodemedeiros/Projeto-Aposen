ea5241199cb3b0ce5b2e694b9a2b8917b9246695
Thiago Medeiros <thiagotitico1@gmail.com>
2026-04-13 00:56:35 -0300

    Criando um data para as noticias q irão no site e fazendo ajuste no header para deixar melhor espaçado

---
+++ data/News.tsx
@@ -0,0 +1,27 @@
+interface News {
+    id: number;
+    title: string;
+    type: string;
+    img : string;
+}
+
+export const news: News[] = [
+    {
+        id: 1,
+        title: "Post 1",
+        type: "Tech",
+        img : "https://img.freepik.com/fotos-gratis/fundo-abstrato-escuro_1048-1920.jpg?semt=ais_hybrid&w=740&q=80"
+    },
+    {
+        id: 1,
+        title: "Post 1",
+        type: "Tech",
+        img : "https://img.freepik.com/fotos-gratis/fundo-abstrato-escuro_1048-1920.jpg?semt=ais_hybrid&w=740&q=80"
+    },
+    {
+        id: 1,
+        title: "Post 1",
+        type: "Tech",
+        img : "https://img.freepik.com/fotos-gratis/fundo-abstrato-escuro_1048-1920.jpg?semt=ais_hybrid&w=740&q=80"
+    },
+]
